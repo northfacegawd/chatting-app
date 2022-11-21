ddb999ecd4d53993ef6cdfcf16166cfec5178252
northfacegawd <chuck5732590@gmail.com>
2022-11-21 20:22:26 +0900

    chore: react query 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,29 @@
 import type { AppProps } from 'next/app';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  DehydratedState,
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>) {
+  const [client] = useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={client}>
+      <Hydrate state={pageProps?.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
