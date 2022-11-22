999fa3687cffaa87cc6916d4ccea0e1a6bd221e1
northfacegawd <chuck5732590@gmail.com>
2022-11-23 00:43:04 +0900

    feat: 채팅 메세지 퍼블리싱

--- components/chat/contents/index.tsx
+++ components/chat/contents/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import ChatMessage from '../message';
 import { ChatContentsWrapper } from './index.style';
 import Write from './write';
 
 export default function ChatContents() {
   return (
     <ChatContentsWrapper>
+      <ChatMessage position="left" />
+      <ChatMessage position="right" />
       <Write />
     </ChatContentsWrapper>
   );
---
+++ components/chat/message/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Avatar from '@mui/material/Avatar';
+
+import { Message, MessageInfo, MessageWrapper, Name } from './index.style';
+
+interface ChatMessageProps {
+  position: 'left' | 'right';
+}
+
+export default function ChatMessage({ position }: ChatMessageProps) {
+  return (
+    <MessageWrapper position={position}>
+      <Avatar />
+      <MessageInfo position={position}>
+        <Name position={position}>최예슬</Name>
+        <Message
+          dangerouslySetInnerHTML={{
+            __html:
+              '안녕하세요 서류지원 합격한 최예슬 입니다.\n우선 좋은소식 감사드립니다.\n제안 주신 일시에 면접이 가능하므로 그 때 뵙겠습니다.',
+          }}
+        />
+      </MessageInfo>
+    </MessageWrapper>
+  );
+}
