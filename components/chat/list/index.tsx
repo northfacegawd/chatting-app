a005fe6003a70d856254b6c13bcf3d739b8be2b8
northfacegawd <chuck5732590@gmail.com>
2022-11-24 21:06:07 +0900

    feat: chat list를 api를 통해서 받아오도록 구현

--- components/chat/list/index.tsx
+++ components/chat/list/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import ChatItem from '@components/chat';
+import useChatList from '@hooks/useChatList';
 
 import { ChatListWrapper } from './index.style';
 
 export default function ChatList() {
+  const { data } = useChatList();
+
   return (
     <ChatListWrapper>
-      {Array.from({ length: 10 }).map((_, i) => (
+      {data?.map((_, i) => (
         <ChatItem key={(i + 1).toString()} />
       ))}
     </ChatListWrapper>
