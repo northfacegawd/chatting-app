62df709ba5bc0581b4cf06ed19a621f09996cd48
northfacegawd <chuck5732590@gmail.com>
2022-11-25 23:56:55 +0900

    fix: chat section 디자인 수정

--- components/chat/list/index.tsx
+++ components/chat/list/index.tsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ChatItem from '@components/chat';
 import useChatList from '@hooks/useChatList';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import SearchIcon from '@mui/icons-material/Search';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import Paper from '@mui/material/Paper';
 
+import { MoreChattingButton, SearchBox } from '../section/index.style';
 import { ChatListWrapper } from './index.style';
 
 export default function ChatList() {
+  const [keyword, setKeyword] = useState('');
   const { data } = useChatList();
 
   return (
-    <ChatListWrapper>
-      {data?.map((_, i) => (
-        <ChatItem key={(i + 1).toString()} />
-      ))}
-    </ChatListWrapper>
+    <>
+      <SearchBox>
+        <Paper
+          component="form"
+          sx={{
+            p: '2px 4px',
+            display: 'flex',
+            alignItems: 'center',
+            width: 400,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="대화 검색하기"
+            inputProps={{ 'aria-label': '대화 검색하기' }}
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        </Paper>
+      </SearchBox>
+      <ChatListWrapper>
+        {Array.from({ length: 10 })?.map((_, i) => (
+          <ChatItem key={(i + 1).toString()} />
+        ))}
+      </ChatListWrapper>
+      <MoreChattingButton endIcon={<ExpandMore />} disabled>
+        이전 대화 불러오기
+      </MoreChattingButton>
+    </>
   );
 }
--- components/chat/section/index.tsx
+++ components/chat/section/index.tsx
@@ -1,40 +1,26 @@
 import React from 'react';
 
 import ChatList from '@components/chat/list';
-import Search from '@components/common/auto-complete';
-import useUserList from '@hooks/useUserList';
 import Add from '@mui/icons-material/Add';
-import ExpandMore from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 
 import {
   ChatCount,
   ChatSectionHeader,
   ChatSectionWrapper,
-  MoreChattingButton,
-  SearchBox,
+  CreateChatButton,
 } from './index.style';
 
 export default function ChatSection() {
-  const { data } = useUserList('chuck5732590');
-
-  console.log(data);
-
   return (
     <ChatSectionWrapper>
       <ChatSectionHeader>
         <ChatCount>안 읽은 대화(2)</ChatCount>
-        <Button variant="contained" startIcon={<Add />}>
+        <CreateChatButton variant="contained" startIcon={<Add />}>
           새로운 메세지
-        </Button>
+        </CreateChatButton>
       </ChatSectionHeader>
-      <SearchBox>
-        <Search />
-      </SearchBox>
       <ChatList />
-      <MoreChattingButton endIcon={<ExpandMore />}>
-        이전 대화 불러오기
-      </MoreChattingButton>
     </ChatSectionWrapper>
   );
 }
