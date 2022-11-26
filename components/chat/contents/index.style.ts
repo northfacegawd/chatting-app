bd37f6c110c13b7b589c4da2c6974da1fa376ded
northfacegawd <chuck5732590@gmail.com>
2022-11-26 23:08:51 +0900

    feat: chat room fade in animation추가

--- components/chat/contents/index.style.ts
+++ components/chat/contents/index.style.ts
@@ -5,4 +5,16 @@ export const ChatContentsWrapper = styled.section`
   width: 100%;
   min-height: calc(100vh - 64px);
   padding: 1rem;
+  animation: fadein 300ms ease-out;
+
+  @keyframes fadein {
+    from {
+      transform: translateY(20%);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0%);
+      opacity: 1;
+    }
+  }
 `;
