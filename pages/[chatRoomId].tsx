f3fbfbd28f353c8e9770ac9e55b4396fceeaf8da
northfacegawd <chuck5732590@gmail.com>
2022-11-27 18:02:51 +0900

    feat: login 했을 때만 접근할 수 있도록 설정

--- pages/[chatRoomId].tsx
+++ pages/[chatRoomId].tsx
@@ -1,4 +1,5 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
+import { getSession } from 'next-auth/react';
 import React from 'react';
 
 import ChatContents from '@components/chat/contents';
@@ -13,3 +14,20 @@ const ChatRoom: NextPage = () => {
 };
 
 export default ChatRoom;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/signin',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
